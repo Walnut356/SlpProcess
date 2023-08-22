d6cf7aaf80da675289104ffedcc3a998adb33360
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-08-22 06:05:06 -0500

    restructured item frame parsing to match the rest of the frame types

--- slpprocess/src/events/item.rs
+++ slpprocess/src/events/item.rs
@@ -1,66 +1,122 @@
+#![allow(clippy::uninit_vec)]
+
 use bytes::{Buf, Bytes};
 use polars::prelude::*;
 
 pub struct Items {
-    frame_number: Vec<i32>,
-    item_id: Vec<u16>,
-    state: Vec<u8>,
-    facing: Vec<f32>,
-    velocity_x: Vec<f32>,
-    velocity_y: Vec<f32>,
-    position_x: Vec<f32>,
-    position_y: Vec<f32>,
-    damage_taken: Vec<u16>,
-    expiration_timer: Vec<f32>,
-    spawn_id: Vec<u32>,
-    missile_type: Vec<Option<u8>>,
-    turnip_type: Vec<Option<u8>>,
-    is_launched: Vec<Option<bool>>,
-    charge_power: Vec<Option<u8>>,
-    owner: Vec<Option<i8>>,
+    frame_number: Box<[i32]>,
+    item_id: Box<[u16]>,
+    state: Box<[u8]>,
+    orientation: Box<[f32]>,
+    velocity_x: Box<[f32]>,
+    velocity_y: Box<[f32]>,
+    position_x: Box<[f32]>,
+    position_y: Box<[f32]>,
+    damage_taken: Box<[u16]>,
+    expiration_timer: Box<[f32]>,
+    spawn_id: Box<[u32]>,
+    missile_type: Box<[Option<u8>]>,
+    turnip_type: Box<[Option<u8>]>,
+    is_launched: Box<[Option<bool>]>,
+    charge_power: Box<[Option<u8>]>,
+    owner: Box<[Option<i8>]>,
 }
 
 impl Items {
     pub fn new(len: usize) -> Self {
         Items {
-            frame_number: Vec::with_capacity(len),
-            item_id: Vec::with_capacity(len),
-            state: Vec::with_capacity(len),
-            facing: Vec::with_capacity(len),
-            velocity_x: Vec::with_capacity(len),
-            velocity_y: Vec::with_capacity(len),
-            position_x: Vec::with_capacity(len),
-            position_y: Vec::with_capacity(len),
-            damage_taken: Vec::with_capacity(len),
-            expiration_timer: Vec::with_capacity(len),
-            spawn_id: Vec::with_capacity(len),
-            missile_type: Vec::with_capacity(len),
-            turnip_type: Vec::with_capacity(len),
-            is_launched: Vec::with_capacity(len),
-            charge_power: Vec::with_capacity(len),
-            owner: Vec::with_capacity(len),
+            frame_number: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            item_id: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            state: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            orientation: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            velocity_x: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            velocity_y: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            position_x: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            position_y: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            damage_taken: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            expiration_timer: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            spawn_id: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            missile_type: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            turnip_type: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            is_launched: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            charge_power: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
+            owner: unsafe {
+                let mut temp = Vec::with_capacity(len);
+                temp.set_len(len);
+                temp.into_boxed_slice()
+            },
         }
     }
 }
 
 #[allow(clippy::from_over_into)]
 impl Into<DataFrame> for Items {
-    fn into(mut self) -> DataFrame {
-        let len = self.frame_number.len();
-
-        // handle potentially optional values
-        if self.missile_type.len() != len {
-            self.missile_type.resize(len, None);
-            self.turnip_type.resize(len, None);
-            self.is_launched.resize(len, None);
-            self.charge_power.resize(len, None);
-        }
-
+    fn into(self) -> DataFrame {
         let vec_series = vec![
             Series::new("frame number", self.frame_number),
             Series::new("item id", self.item_id),
             Series::new("state", self.state),
-            Series::new("facing", self.facing),
+            Series::new("facing", self.orientation),
             Series::new("velocity x", self.velocity_x),
             Series::new("velocity y", self.velocity_y),
             Series::new("position x", self.position_x),
@@ -82,33 +138,35 @@ impl Into<DataFrame> for Items {
 pub fn parse_itemframes(frames: &mut [Bytes]) -> DataFrame {
     let mut working = Items::new(frames.len());
 
-    for frame in frames {
-        working.frame_number.push(frame.get_i32());
-        working.item_id.push(frame.get_u16());
-        working.state.push(frame.get_u8());
-        working.facing.push(frame.get_f32());
-        working.velocity_x.push(frame.get_f32());
-        working.velocity_y.push(frame.get_f32());
-        working.position_x.push(frame.get_f32());
-        working.position_y.push(frame.get_f32());
-        working.damage_taken.push(frame.get_u16());
-        working.expiration_timer.push(frame.get_f32());
-        working.spawn_id.push(frame.get_u32());
+    for (i, frame) in frames.iter_mut().enumerate() {
+        unsafe {
+            *working.frame_number.get_unchecked_mut(i) = frame.get_i32();
+            *working.item_id.get_unchecked_mut(i) = frame.get_u16();
+            *working.state.get_unchecked_mut(i) = frame.get_u8();
+            *working.orientation.get_unchecked_mut(i) = frame.get_f32();
+            *working.velocity_x.get_unchecked_mut(i) = frame.get_f32();
+            *working.velocity_y.get_unchecked_mut(i) = frame.get_f32();
+            *working.position_x.get_unchecked_mut(i) = frame.get_f32();
+            *working.position_y.get_unchecked_mut(i) = frame.get_f32();
+            *working.damage_taken.get_unchecked_mut(i) = frame.get_u16();
+            *working.expiration_timer.get_unchecked_mut(i) = frame.get_f32();
+            *working.spawn_id.get_unchecked_mut(i) = frame.get_u32();
 
-        if !frame.has_remaining() {
-            // version < 3.2.0
-            continue;
-        }
-        working.missile_type.push(Some(frame.get_u8()));
-        working.turnip_type.push(Some(frame.get_u8()));
-        working.is_launched.push(Some(frame.get_u8() != 0));
-        working.charge_power.push(Some(frame.get_u8()));
+            if !frame.has_remaining() {
+                // version < 3.2.0
+                continue;
+            }
+            *working.missile_type.get_unchecked_mut(i) = Some(frame.get_u8());
+            *working.turnip_type.get_unchecked_mut(i) = Some(frame.get_u8());
+            *working.is_launched.get_unchecked_mut(i) = Some(frame.get_u8() != 0);
+            *working.charge_power.get_unchecked_mut(i) = Some(frame.get_u8());
 
-        if !frame.has_remaining() {
-            // version < 3.6.0
-            continue;
+            if !frame.has_remaining() {
+                // version < 3.6.0
+                continue;
+            }
+            *working.owner.get_unchecked_mut(i) = Some(frame.get_i8());
         }
-        working.owner.push(Some(frame.get_i8()));
     }
 
     working.into()
