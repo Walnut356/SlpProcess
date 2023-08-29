b84615cfe8bc74de4a0434498c4587831db9e36e
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-08-29 06:39:25 -0500

    fix get_player functions

--- slpprocess/src/game.rs
+++ slpprocess/src/game.rs
@@ -1,10 +1,11 @@
 use std::path::Path;
-use std::sync::{Arc, RwLock};
-use std::time::Duration;
+use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
+use std::time::{Duration, Instant};
 
-use anyhow::{ensure, Result, anyhow};
+use anyhow::{anyhow, ensure, Result};
 use polars::prelude::DataFrame;
 
+use crate::stats::helpers::get_stats;
 use crate::Port;
 use crate::{
     events::{
@@ -34,12 +35,24 @@ impl Game {
     pub fn new(path: &Path) -> Result<Self> {
         ensure!(path.is_file() && path.extension().unwrap() == "slp");
         let file_data = Self::get_file_contents(path)?;
-        Game::parse(file_data)
+        let mut game = Game::parse(file_data)?;
+        // let now = Instant::now();
+        get_stats(&mut game);
+        // let dur = now.elapsed();
+        // println!("{:?}", dur);
+
+        Ok(game)
     }
 
-    pub fn get_port(&self, port: Port) -> Result<&RwLock<Player>> {
-        for player in self.players.as_ref().iter() {
-            if player.read().unwrap().port == port {
+    pub fn player_by_port(&self, port: Port) -> Result<RwLockReadGuard<'_, Player>> {
+        for p_lock in self.players.as_ref().iter() {
+            // TODO i don't like making this a string, but RwLock errors require Sync/Send so
+            // they can't be directly converted to anyhow errors without some fiddling. I don't
+            // feel super confident about that, but it's also not very important atm so i'll
+            // look into it later
+            let player = p_lock.read().map_err(|x| anyhow!("{:?}", x.to_string()))?;
+
+            if player.port == port {
                 return Ok(player);
             }
         }
@@ -47,9 +60,29 @@ impl Game {
         Err(anyhow!("Unable to find player with port {:?}", port))
     }
 
-    pub fn get_port_mut(&mut self, port: Port) -> Result<&RwLock<Player>> {
-        for player in self.players.iter() {
-            if player.write().unwrap().port == port {
+    pub fn player_by_code(&self, connect_code: &str) -> Result<RwLockReadGuard<'_, Player>> {
+        for p_lock in self.players.iter() {
+            let player = p_lock.read().map_err(|x| anyhow!("{:?}", x.to_string()))?;
+
+            if player.connect_code.as_ref().is_some_and(|x| x.as_str() == connect_code) {
+                return Ok(player);
+            }
+        }
+
+        Err(anyhow!(
+            "Unable to find player with connect code '{:?}'",
+            connect_code
+        ))
+    }
+
+    pub(crate) fn player_by_port_mut(
+        &mut self,
+        port: Port,
+    ) -> Result<RwLockWriteGuard<'_, Player>> {
+        for p_lock in self.players.iter() {
+            let player = p_lock.write().map_err(|x| anyhow!("{:?}", x.to_string()))?;
+
+            if player.port == port {
                 return Ok(player);
             }
         }
