600634310e2161fb7900773d1535feccded2d2fb
Walnut <39544927+Walnut356@users.noreply.github.com>
2023-09-03 03:07:22 -0500

    added helper functions

--- slpprocess/src/stats/helpers.rs
+++ slpprocess/src/stats/helpers.rs
@@ -2,44 +2,47 @@ use itertools::Itertools;
 
 use crate::enums::general::*;
 use crate::enums::stage::Stage;
-use crate::stats::inputs::find_inputs;
+use crate::enums::{buttons::*, general::*, state::*};
 use crate::utils::BitFlags;
-use crate::{
-    enums::{buttons::*, general::*, state::*},
-    Game,
-};
+use crate::enums::state::ActionRange as AR;
 
-use super::defense::find_defense;
-use super::items::find_items;
-use super::lcancel::find_lcancels;
-
-pub fn get_stats(game: &mut Game) {
-    for players in game.players.iter().permutations(2) {
-        let mut player = players[0].as_ref().write().unwrap();
-        let opponent = players[1].as_ref().read().unwrap();
-        let items = &game.item_frames;
-
-        player.stats.l_cancel = find_lcancels(&player.frames, Stage::from_id(game.start.stage));
-        player.stats.actions = find_inputs(&player.frames, game.total_frames);
-        player.stats.items = find_items(&player.frames, player.port, items);
-        player.stats.defense = find_defense(&player.frames, &opponent.frames);
-    }
+/// takes `PostFrames.action_state` and a frame index, returns true if
+pub fn just_changed_state(states: &[u16], index: usize) -> bool {
+    states[index] != states[index - 1]
 }
 
-pub fn just_input_lcancel(frames: &[u32], i: usize) -> bool {
-    let current = EngineInput::from(frames[i]);
-    let previous = EngineInput::from(frames[i.saturating_sub(1)]);
+pub fn just_entered_state(target: ActionState, states: &[u16], index: usize) -> bool {
+    states[index] == target as u16 && states[index - 1] != target as u16
+}
 
-    let mask: u32 = (EngineInput::Z | EngineInput::ANY_TRIGGER).into();
+pub fn just_exited_state(target: ActionState, states: &[u16], index: usize) -> bool {
+    states[index] != target as u16 && states[index - 1] == target as u16
+}
 
-    current.intersects(mask) && !previous.intersects(mask)
+pub fn just_input_lcancel(frames: &[u32], index: usize) -> bool {
+    let current = EngineInput::from(frames[index]);
+    let previous = EngineInput::from(frames[index.saturating_sub(1)]);
+
+    let mask: u32 = EngineInput::ANY_TRIGGER.into();
+
+    current.contains(mask) && !previous.contains(mask)
+}
+
+pub fn just_took_damage(percent_frames: &[f32], index: usize) -> bool {
+    percent_frames[index] > percent_frames[index - 1]
 }
 
 pub fn is_in_hitlag(flags: u64) -> bool {
     Flags::from(flags).contains(Flags::HITLAG.into())
 }
 
+pub fn is_in_hitstun(flags: u64) -> bool {
+    Flags::from(flags).contains(Flags::HITSTUN.into())
+}
 
+pub fn is_shielding(flags: u64) -> bool {
+    Flags::from(flags).contains(Flags::SHIELDING.into())
+}
 
 pub fn is_fastfalling(flags: u64) -> bool {
     Flags::from(flags).contains(Flags::FASTFALL.into())
@@ -47,7 +50,28 @@ pub fn is_fastfalling(flags: u64) -> bool {
 
 /// Returns true if the player is in any tumble or reeling animation, or if they are in the jab reset animation
 pub fn is_damaged(state: u16) -> bool {
-    (ActionRange::DAMAGE_START as u16 <= state && state <= ActionRange::DAMAGE_END as u16)
+    (AR::DAMAGE_START as u16..=AR::DAMAGE_END as u16).contains(&state)
         || state == ActionState::DOWN_DAMAGE_D as u16
         || state == ActionState::DOWN_DAMAGE_U as u16
+}
+
+pub fn is_grabbed(state: u16) -> bool {
+    (AR::CAPTURE_START as u16..=AR::CAPTURE_END as u16).contains(&state)
+}
+
+pub fn is_cmd_grabbed(state: u16) -> bool {
+    state != ActionState::BARREL_WAIT as u16
+        && ((AR::COMMAND_GRAB_RANGE1_START as u16
+            ..=AR::COMMAND_GRAB_RANGE1_END as u16)
+            .contains(&state)
+            || (AR::COMMAND_GRAB_RANGE2_START as u16
+                ..=AR::COMMAND_GRAB_RANGE2_END as u16)
+                .contains(&state))
+}
+
+pub fn is_teching(state: u16) -> bool {
+    (AR::TECH_START as u16..=AR::TECH_END as u16).contains(&state)
+        || (AR::DOWN_START as u16..=AR::DOWN_END as u16).contains(&state)
+        || state == ActionState::FLY_REFLECT_CEIL as u16
+        || state == ActionState::FLY_REFLECT_WALL as u16
 }
